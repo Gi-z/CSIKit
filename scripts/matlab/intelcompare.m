ae043d213e6e05d7c7e0f63e7dded1a8dda0e4b7
Glenn <gizmoloon@gmail.com>
2020-08-31 16:45:33 +0100

    Added comparison script for MATLAB.

--- scripts/matlab/intelcompare.m
+++ scripts/matlab/intelcompare.m
@@ -1,13 +1,43 @@
 invalid = false;
 
-for t=1:size(csi)
-   test = csi{t}.csi == csi_trace{t}.csi;
-   if all(test(:)==1)
-       continue;
-   else
-       disp(t);
-       invalid = true;
-   end
+tests = ["log.all_csi.6.7.6", "brushteeth_1590158472", "cook_1590161749", "sleeping_post_1597163585", "walk_1590161182"];
+scaledTest = false;
+
+for i=1:length(tests)
+    test = tests{i};
+    
+    disp(fprintf("Testing: %s", test));
+   
+    intelCSIStruct = read_bf_file(sprintf("sample_data/%s.dat", test));
+    pythonCSIStruct = load(sprintf("sample_data/%s.mat", test));
+   
+    for t=1:size(intelCSIStruct)
+       intelMat = intelCSIStruct{t};
+       if ~isempty(intelMat)
+           
+           if scaledTest == true
+               intelMat = get_scaled_csi(intelMat);
+               pythonMat = permute(pythonCSIStruct.csi{t}.scaled_csi, [3 2 1]);
+           else
+               intelMat = intelMat.csi;
+               pythonMat = permute(pythonCSIStruct.csi{t}.csi, [3 2 1]);
+           end
+           
+           %Appears to be some form of rounding error.
+           %Direct equality checks fail even though they visually appear
+           %identical in MATLAB itself.
+           %Instead, we'll use an equality check with a wider tolerance.
+           test = abs(intelMat-pythonMat) < 1e4*eps(min(abs(intelMat),abs(pythonMat)));
+           
+           if all(test(:)==1)
+               testNo = testNo+1;
+               continue;
+           else
+               disp(t);
+               invalid = true;
+           end
+       end
+    end
 end
 
 if invalid == false
