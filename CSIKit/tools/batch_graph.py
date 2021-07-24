fc21fb55971923f2be6f9e2a5d9d32527b13b291
giz <gizmoloon@gmail.com>
2021-07-24 17:43:48 +0100

    - Resolved original default for heatmap graph view.

--- CSIKit/tools/batch_graph.py
+++ CSIKit/tools/batch_graph.py
@@ -63,7 +63,7 @@ class BatchGraph:
         if len(finalEntry.shape) == 4:
             #>1 antenna stream.
             #Loading the first for ease.
-            finalEntry = finalEntry[:, :, 3, 0]
+            finalEntry = finalEntry[:, :, 0, 0]
 
         # from CSIKit.filters.wavelets.dwt import denoise
         # finalEntry = denoise(finalEntry)
